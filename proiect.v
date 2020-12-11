bb70a74d7e1aba25e97c5026e541908da0bba3d6
mihaela867 <mihaelachira8@gmail.com>
2020-12-11 19:11:15 +0200

    minor updates for switch case, struct

--- proiect.v
+++ proiect.v
@@ -1,15 +1,17 @@
 (*Variables and environment*)
 Require Import String.
 
-Inductive ValueTypes:=
-| natural : nat -> ValueTypes
-| boolean : bool -> ValueTypes
-| String : string -> ValueTypes.
+Inductive Value:=
+| undeclared : Value
+| defined : Value
+| natural : nat -> Value
+| boolean : bool -> Value
+| String : string -> Value.
 
-Scheme Equality for ValueTypes.
+Scheme Equality for Value.
 
-Definition Env := string -> ValueTypes.
-
+Definition Env := string -> Value.
+Definition Memory := nat -> Value.
 (* Arithmetic expressions *)
 
  Inductive AExp :=
@@ -21,7 +23,7 @@ Definition Env := string -> ValueTypes.
 | adiv : AExp -> AExp -> AExp
 | amod : AExp -> AExp -> AExp.
 
- 
+
 Coercion avar : string >-> AExp.
 Coercion anum : nat >-> AExp.
 Notation "A +' B" := (aplus A B) (at level 48, right associativity).
@@ -48,6 +50,8 @@ Infix "and'" := band (at level 80).
 Infix "or'" := bor (at level 80).
 
 (*Statements*)
+Require Import Coq.Lists.List.
+Import ListNotations.
 
 Inductive Stmt :=
 | assignment : string -> AExp -> Stmt
@@ -56,37 +60,47 @@ Inductive Stmt :=
 | ifthenelse : BExp -> Stmt -> Stmt -> Stmt
 | while : BExp -> Stmt -> Stmt
 | For : Stmt -> BExp -> Stmt -> Stmt->Stmt.
-
+(* 
 Inductive List:=
 | nil
-| cons (n:ValueTypes)(l: List)
-| cons' (exp : Stmt) (l:List).
+| cons (n:Value)(l: List)
+| cons' (exp : Stmt) (l:List). *)
+
+Inductive cases :=
+| case : AExp -> Stmt -> cases
+| defaultCase : Stmt -> cases.
 
 Inductive switch :=
-| switchCase: AExp -> List -> List -> switch.
+| switchCase: AExp -> list cases -> switch.
 
 
 Notation "X ::= A" := (assignment X A) (at level 50).
 Notation "S1 ;; S2" := (sequence S1 S2) (at level 90).
 Notation "'If' B 'Then' S1 'Else' S2 'End'" := (ifthenelse B S1 S2) (at level 97).
 
-
 Check ifthen (3<'2) ("a" ::= 3).
 Check 1+'2.
-Check switchCase (3+'2) (cons (natural 6) (nil)) (cons' ("a" ::= 4)(nil)).
-Check switchCase (3+'2) (cons (natural 6) ((cons (natural 7) (nil))))  (cons' ("a" ::= 4)((cons' ("b" ::= 4)(nil)))). 
+Compute [ 1 ].
+Check switchCase (1+'2) [defaultCase ("a" ::= 2)].
+Check switchCase (avar "a")
+    [case (5) ("a" ::= 4);
+     defaultCase ("a" ::= 0)].
 
 (* String operations*)
 
 Inductive StringOp :=
 | concat: string -> string -> StringOp
 | compare: string -> string -> StringOp
-| length: string -> StringOp
+| Slength: string -> StringOp
 | Sassignment: string -> string -> StringOp.
+
+Check concat "ana" "maria".
+
 
 (*Arrays*)
 
-Definition Array:= nat -> ValueTypes.
+(* ++declare arrays??? *)
+Definition Array:= nat -> Value.
 
 Definition array0 : Array :=
   fun x =>
@@ -96,3 +110,51 @@ Definition array0 : Array :=
 
 Compute array0 0.
 Compute array0 1.
+
+Inductive ArrayOp :=
+| Alength : Array -> ArrayOp
+| push: Array -> Value -> ArrayOp
+| delete: Array -> nat -> ArrayOp
+| pop: Array -> ArrayOp.
+
+Check Alength array0.
+
+
+(* Struct *)
+
+Inductive Members := 
+| member : Type -> string -> Value -> Members.
+
+Inductive struct := 
+| Struct : (list Members) -> struct.
+
+Compute Struct [member nat "a" defined].
+Compute Struct [member bool "b" (boolean true); 
+                member nat "x" (defined)].
+
+
+(* Inductive Var :=
+| defaultV
+| naturalv : string ->nat->Var.
+
+Inductive memberList:=
+| nill
+| member (t:Type) (v:Var).
+
+(* Inductive struct:= 
+|members: string -> memberList->struct.
+ *)
+Definition struct := string->memberList.
+Definition struct1 (s:string) ()  : struct :=
+    
+
+Definition Env := Var -> nat.
+Definition env1 : Env :=
+  fun x =>
+    if (Var_eq_dec x n)
+    then 10
+    else if (Var_eq_dec x x)
+    then 15
+    else 0. *)
+
+(* pointeri si referinte*)
